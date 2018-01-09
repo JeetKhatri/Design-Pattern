6fc208d74cca3fd2f8664e7aba3fc7579f70621e
Jeet Khatri <jeet.g.khatri@gmail.com>
2018-01-09 12:07:44 +0530

    Factory Design Pattern

---
+++ src/FactoryBookTicketGold.java
@@ -0,0 +1,17 @@
+/***
+ * 
+ * @author JeetKhatri
+ * @since Jan,18
+ *
+ */
+
+public class FactoryBookTicketGold extends FactoryBookTicketAbstractClass {
+	
+	/***
+	 * @return Price of 1 ticket.
+	 */
+	@Override
+	public int getPrice() {
+		return 150;
+	}
+}
---
+++ src/FactoryBookTicketSilver.java
@@ -0,0 +1,15 @@
+/***
+ * 
+ * @author JeetKhatri
+ * @since Jan,18
+ *
+ */
+public class FactoryBookTicketSilver extends FactoryBookTicketAbstractClass {
+	/***
+	 * @return Price of 1 ticket.
+	 */
+	@Override
+	public int getPrice() {
+		return 100;
+	}
+}
